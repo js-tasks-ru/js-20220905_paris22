cb544e26b8e09e6d8dc9133a86bf0c8b83bd750a
Vladislav Latanov <lvn1722@yandex.ru>
2022-09-12 10:01:00 +0300

    задачи про примитивные типы данных

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -5,5 +5,12 @@
  * @returns {string[]}
  */
 export function sortStrings(arr, param = 'asc') {
+  const sortableArr = [...arr];
+  sortableArr.sort((str1, str2) =>
+    str1.localeCompare(str2, ['ru', 'en'], {caseFirst: 'upper'})
+  );
 
+  if (param === 'desc') return sortableArr.reverse();
+
+  return sortableArr;
 }
