b742f7d295bd1954b3389a1541a4b5f5e4636821
Vladislav Latanov <lvn1722@yandex.ru>
2022-09-26 09:35:25 +0300

    4,5,6 tasks

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,64 @@
 export default class NotificationMessage {
+  element;
 
+  constructor(
+    message = "",
+    {
+      duration = 0,
+      type = "error"
+    } = {}) {
+    this.message = message;
+    this.duration = duration;
+    this.type = type;
+
+    this.render();
+  }
+
+  get template() {
+    return `
+      <div class="notification ${this.type}" style="--value:${this.duration / 1000}s">
+        <div class="timer"></div>
+        <div class="inner-wrapper">
+          <div class="notification-header">${this.type}</div>
+          <div class="notification-body">
+            ${this.message}
+          </div>
+        </div>
+      </div>
+    `;
+  }
+
+  render() {
+    const wrapper = document.createElement("div");
+
+    wrapper.innerHTML = this.template;
+
+    this.element = wrapper.firstElementChild;
+  }
+
+  show(parent = document.body) {
+    if (NotificationMessage.activeNote) {
+      NotificationMessage.activeNote.remove();
+    }
+
+    parent.append(this.element);
+
+    setTimeout(() => {
+      this.remove();
+    }, this.duration);
+
+    NotificationMessage.activeNote = this;
+  }
+
+  destroy() {
+    this.remove();
+    this.element = null;
+    this.duration = 0;
+  }
+
+  remove() {
+    if (this.element) {
+      this.element.remove();
+    }
+  }
 }
--- 06-events-practice/1-sortable-table-v2/index.js
+++ 06-events-practice/1-sortable-table-v2/index.js
@@ -1,8 +1,180 @@
 export default class SortableTable {
-  constructor(headersConfig, {
-    data = [],
-    sorted = {}
-  } = {}) {
+  subElements = {};
+  isSortLocally = true;
 
+  constructor(headersConfig, { data = [], sorted = {} } = {}) {
+    this.headersConfig = headersConfig;
+    this.data = [...data];
+    this.sorted = sorted;
+
+    this.render();
+    this.sort(sorted.id, sorted.order);
+  }
+
+  sort(fieldValue, orderValue) {
+    if (this.isSortLocally) {
+      this.sortOnClient(fieldValue, orderValue);
+    } else {
+      this.sortOnServer();
+    }
+
+    this.update();
+  }
+
+  sortOnServer(fieldValue, orderValue) {
+    return;
+  }
+
+  sortOnClient(fieldValue, orderValue) {
+    const sortingField = this.element.querySelector(
+      `[data-id='${fieldValue}']`
+    );
+
+    let direction;
+    if (orderValue === "asc") {
+      direction = 1;
+      sortingField.dataset.order = orderValue;
+    }
+    if (orderValue === "desc") {
+      direction = -1;
+      sortingField.dataset.order = orderValue;
+    }
+
+    this.data.sort((a, b) => {
+      if (typeof a[fieldValue] === "number") {
+        return direction * (a[fieldValue] - b[fieldValue]);
+      }
+      if (typeof a[fieldValue] === "string") {
+        return (
+          direction *
+          a[fieldValue].localeCompare(b[fieldValue], ["ru", "en"], {
+            caseFirst: "upper",
+          })
+        );
+      }
+    });
+  }
+
+  renderProductCells(product) {
+    return this.headersConfig
+      .map((data) => {
+        return data.template
+          ? data.template()
+          : `<div class="sortable-table__cell">${product[data.id]}</div>`;
+      })
+      .join("");
+  }
+
+  renderProducts() {
+    return this.data
+      .map((product) => {
+        return `
+          <a href="/products/${product.id}" class="sortable-table__row">
+            ${this.renderProductCells(product)}
+          </a>
+        `;
+      })
+      .join("");
+  }
+
+  get tableRows() {
+    return `
+      <span data-element="arrow" class="sortable-table__sort-arrow">
+        <span class="sort-arrow"></span>
+      </span>
+    `;
+  }
+
+  renderHeader() {
+    return this.headersConfig
+      .map((cell) => {
+        return `
+        <div
+          class="sortable-table__cell"
+          data-id="${cell.id}"
+          data-sortable="${cell.sortable}"
+          data-order=""
+        >
+          <span>${cell.title}</span>
+          ${cell.sortable ? this.tableRows : ""}
+        </div>
+      `;
+      })
+      .join("");
+  }
+
+  get template() {
+    return `
+      <div data-element="productsContainer" class="products-list__container">
+        <div class="sortable-table">
+          <div data-element="header" class="sortable-table__header sortable-table__row">
+            ${this.renderHeader()}
+          </div>
+        </div>
+        <div data-element="body" class="sortable-table__body">
+          ${this.renderProducts()}
+        </div>
+        <div data-element="loading" class="loading-line sortable-table__loading-line"></div>
+          <div data-element="emptyPlaceholder" class="sortable-table__empty-placeholder">
+            <div>
+              <p>No products satisfies your filter criteria</p>
+              <button type="button" class="button-primary-outline">Reset all filters</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    `;
+  }
+
+  render() {
+    if (this.element) this.remove();
+    const element = document.createElement("div");
+    element.innerHTML = this.template;
+    this.element = element.firstElementChild;
+    this.subElements = this.getSubElements();
+
+    this.initListeners();
+  }
+
+  validateSort(e) {
+    const headerItem  = e.target.closest(".sortable-table__cell");
+    if (headerItem ?.dataset?.sortable === "true") {
+      const order = headerItem .dataset.order === "asc" ? "asc" : "desc";
+      this.sort(headerItem .dataset.id, order);
+    }
+  }
+
+  initListeners() {
+    const contextValidateSort = this.validateSort.bind(this);
+    document.addEventListener("pointerdown", contextValidateSort);
+    this.removeEventListeners = () => {
+      document.removeEventListener("pointerdown", contextValidateSort);
+    };
+  }
+
+  getSubElements() {
+    const subElements = {};
+    const elements = this.element.querySelectorAll("[data-element]");
+
+    for (const subElement of elements) {
+      const name = subElement.dataset.element;
+
+      subElements[name] = subElement;
+    }
+    return subElements;
+  }
+
+  update() {
+    this.element.innerHTML = this.template;
+    this.subElements = this.getSubElements();
+  }
+
+  remove() {
+    this.element.remove();
+  }
+
+  destroy() {
+    this.element.remove();
+    this.subElements = {};
   }
 }
